6a364167fb6119910e4ad3a90014c64837eadabe
Hélio Potelicki <51384974+HELIOPOTELICKI@users.noreply.github.com>
2022-07-22 20:27:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/' 'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
 def load_data(nrows):
